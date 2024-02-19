0e61bdb498f39cfb3b260c15db30cabcc00274b5
Ashish Bhawel <devopstazz@gmail.com>
2024-02-19 18:50:16 +0530

    Delete Final-static-file directory

--- Final-static-file/wp-content/uploads/uag-plugin/assets/2000/uag-js-1605.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBCounter.init( '.uagb-block-4db46b14', {"layout":"number","heading":"","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":1,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
-});
-window.addEventListener( 'load', function() {
-	UAGBCounter.init( '.uagb-block-83479056', {"layout":"number","heading":"","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":10,"totalNumber":120,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
-});
- });
